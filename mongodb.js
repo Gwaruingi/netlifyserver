45edd41d4a3d7a83fb564d3e40d5fca104c860e1
Geoffrey <gwaruingi@gmail.com>
2023-02-18 19:38:48 +0300

    adding mongodb

--- mongodb.js
+++ mongodb.js
@@ -18,25 +18,36 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Andrew',
-        age: 27
-    })
+//     db.collection('users').insertOne({
+//         _id: id,
+//         name: 'Andrew',
+//         age: 27
+//     })
 
-    db.collection('tasks').insertMany([
- {
- description: 'Clean the house',
- completed: true
- },{
- description: 'Renew inspection',
- completed: false
- }
-], (error, result) => {
- if (error) {
- return console.log('Unable to insert tasks!')
- }
- console.log(result.ops)
+//     db.collection('tasks').insertMany([
+//  {
+//  description: 'Clean the house',
+//  completed: true
+//  },{
+//  description: 'Renew inspection',
+//  completed: false
+//  }
+// ], (error, result) => {
+//  if (error) {
+//  return console.log('Unable to insert tasks!')
+//  }
+//  console.log(result.ops)
+// })
+
+db.collection('tasks').findOne({description: 'Renew inspection'}, (error, task) => {
+
+    if (error) {
+        return console.log('record not found')
+    }
+
+    console.log(task)
+
 })
+
     
 })
